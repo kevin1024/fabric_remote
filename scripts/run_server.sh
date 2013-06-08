6d976d742af5e67ddae2ca2d96defc6788bfda87
Lazy Live Server Person <support@realgeeks.com>
2013-06-08 00:27:57 +0000

    increase gunicorn timeout

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval `ssh-agent`
 ssh-add /var/apps/.ssh/id_rsa
-exec /var/apps/.virtualenvs/furoshiki/bin/gunicorn furoshiki:app -t 300 -b unix:/var/apps/socks/furoshiki.sock
+exec /var/apps/.virtualenvs/furoshiki/bin/gunicorn furoshiki:app -t 3000 -b unix:/var/apps/socks/furoshiki.sock
