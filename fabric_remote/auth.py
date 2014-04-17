4abf8363b7e8e1b20f0d06ede3da3518a011fa2c
Kevin McCarthy <me@kevinmccarthy.org>
2014-04-16 20:31:14 -1000

    no auth for options

--- fabric_remote/auth.py
+++ fabric_remote/auth.py
@@ -38,7 +38,7 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
+        if (not auth or not check_auth(auth.username, auth.password)) and not request.method == 'OPTIONS':
             return authenticate()
         return f(*args, **kwargs)
     return decorated
