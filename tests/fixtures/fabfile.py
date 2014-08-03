f53dc4255da97890d6060b245e5ed9f4a4d1d345
Kevin McCarthy <me@kevinmccarthy.org>
2014-08-03 11:51:07 -1000

    dont connect to a remote host in the fixture fabfile

--- tests/fixtures/fabfile.py
+++ tests/fixtures/fabfile.py
@@ -1,22 +1,21 @@
 import os
 from fabric.api import task, run, local, env
 import time
-env.hosts = [os.environ.get("TEST_HOST")]
 
 @task
 def host_type():
     """
     test description
     """
-    run('uname -a')
-    run('ls -l /usr/lib')
-    run('echo blah')
+    local('uname -a')
+    local('ls -l /usr/lib')
+    local('echo blah')
     time.sleep(4)
-    run('echo blah')
+    local('echo blah')
     time.sleep(4)
-    run('echo blah')
+    local('echo blah')
     time.sleep(4)
-    run('echo blah')
+    local('echo blah')
     env['foo'] = 'bar'
     return "shit worked"
 
